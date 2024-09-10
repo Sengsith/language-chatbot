f23f6a089d223da34fcd535ef854b71dc31a97d5
Angelo Rosario <angelorosario10@gmail.com>
2024-09-10 16:30:00 -0700

    update App.tsx
    added text input field for user to type prompts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,27 @@
+import { useRef } from "react";
 import useGemini from "./hooks/useGemini";
 import Loading from "./components/Loading";
+import { FormEvent } from "react";
 
 const App = () => {
+  const promptRef = useRef<HTMLInputElement>(null);
   const { loading, sendPrompt, isChatEmpty, getChatHistory } = useGemini();
 
-  const handleOnClick = () => {
-    console.log("click");
-    sendPrompt("I have 2 dogs in my house");
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!promptRef.current) return;
+    console.log("submit");
+    sendPrompt(promptRef.current.value);
+    promptRef.current.value = "";
   };
 
   return (
     <div id="app" className="bg-neutral-800 text-white min-h-screen">
       <h1 className="text-4xl">Language Chatbot</h1>
-      <button onClick={handleOnClick}>Send prompt</button>
+      <form action="" onSubmit={handleSubmit}>
+        <input ref={promptRef} className="text-black" type="text" />
+        <button type="submit">Send prompt</button>
+      </form>
       {loading && <Loading />}
       {!isChatEmpty() &&
         getChatHistory().map((message, index) => {
